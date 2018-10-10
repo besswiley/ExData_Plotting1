8fd7f030bce4a7573b596f034c34d86c203a60c8
besswiley <42952743+besswiley@users.noreply.github.com>
2018-10-09 19:13:05 -0700

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,9 @@
+data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dateData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+dateTime <- strptime(paste(dateData$Date, dateData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(dateData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(dateTime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
